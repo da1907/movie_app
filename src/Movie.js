14e5770f573b129cf6f6f9dc812e936ad263361b
da1907 <cd03252@naver.com>
2020-11-11 20:40:56 +0900

    #2.0 Link to movie API and show data

---
+++ src/Movie.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+// state가 필요없기 때문에 class가 아닌 function으로 구현
+function Movie({ year, title, summary, poster }) {
+  return (
+    <div class="movie">
+      <img src={poster} alt={title} title={title} />
+      <div class="movie__data">
+        <h3 class="movie__title">{title}</h3>
+        <h5 class="movie__year">{year}</h5>
+        <p class="movie__summary">{summary}</p>
+      </div>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+};
+
+export default Movie;
